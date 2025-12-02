9c7e883939e386500ae8723c3d34fc00bd9c68fb
LAPTOP-8JDDPDSK\phucb <phucbui5655@gmail.com>
2025-12-02 09:30:38 +0700

    fix endpoint

--- src/env/apiEndpoint.js
+++ src/env/apiEndpoint.js
@@ -2,7 +2,7 @@
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 export const API_ENDPOINTS = {
-  LOGIN: `${API_BASE_URL}/auth/login`,
+  LOGIN: `${API_BASE_URL}/authentications`,
   CONFIRM_EMAIL: `${API_BASE_URL}/confirmations`,
   FORGOT_PASSWORD: `${API_BASE_URL}/auth/forgot-password`,
 };
